f3c08e1a10d06a87a1c12d18e2e3e9c9a21dc39f
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-05-03 17:50:02 -0700

    configuring reviews

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -2,22 +2,22 @@ class Api::V1::JobsController < ApplicationController
   rescue_from StandardError, with: :handle_unexpected_error
   rescue_from JSON::ParserError, with: :handle_json_parsing_error
 
-  physicians = [
-    "Dr. Ron Bowman",
-    "Dr. Alex Friedman",
-    "Dr. Clifford D. Mah",
-    "Dr. Denny Le",
-    "Dr. Jason Surratt",
-    "Dr. Manny Moy",
-    "Dr. Mia Horvath",
-    "Dr. Peter Pham",
-    "Dr. Thomas Melillo",
-    "Dr. Todd Galle",
-    "Dr. Yama Dehqanzada",
-    "Dr. Cara Beach",
-    "Dr. Lacey Beth Lockhart",
-    "Dr. Melinda Nicholes",
-    "Dr. Taylor Bunka"
+  PHYSICIANS = [
+    "Bowman",
+    "Friedman",
+    "Mah",
+    "Le",
+    "Surratt",
+    "Moy",
+    "Horvath",
+    "Pham",
+    "Melillo",
+    "Galle",
+    "Dehqanzada",
+    "Beach",
+    "Lockhart",
+    "Nicholes",
+    "Bunka"
   ]
 
   def index
@@ -27,39 +27,36 @@ class Api::V1::JobsController < ApplicationController
 
   def pull_google_places_cache
     puts "Pulling and filtering Google Places cache..."
-
     log_daily_visits
     csrf_token = form_authenticity_token
-
     reviews_json = GooglePlacesCached.cached_google_places_reviews
     reviews = JSON.parse(reviews_json)
 
     if reviews.blank?
       OfficeMailer.alert_no_reviews_email.deliver_later
     else
-      puts "Found #{reviews.length} reviews."
-      reviews.each do |review|
-        # Check if the review is from one of the specified physicians
-        next unless physicians.include?(review['author_name'])
-        
-        # You can access filtered reviews here
-        puts "Name: #{review['author_name']}"
-        puts "Rating: #{review['rating']}"
-        puts "Text: #{review['text']}"
-        puts "-------------------------"
-      end
+      formatted_reviews = format_reviews(reviews)
+      puts formatted_reviews.join("\n-------------------------\n")
     end
-    puts "Finished pulling and filtering Google Places cache."
 
     render json: { reviews: reviews, csrf_token: csrf_token }
   end
 
   private
 
+  def format_reviews(reviews)
+    reviews.filter_map do |review|
+      author_last_name = review['author_name'].gsub(/Dr\.?\s*/, '').split.last # Remove "Dr." and "Dr" prefixes and get the last name
+      next unless PHYSICIANS.include?(author_last_name)
+
+      "Name: #{review['author_name']}\nRating: #{review['rating']}\nText: #{review['text']}"
+    end
+  end
+
   def log_daily_visits
     today = Date.today
     visits_key = "daily_visits_#{today}"
-    redis = Redis.new(url: ENV['REDIS_URL'])
+    redis = Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER })
     current_visits = redis.incr(visits_key)
     puts "*" * 100
     puts "Daily visits on #{today}: #{current_visits}"
