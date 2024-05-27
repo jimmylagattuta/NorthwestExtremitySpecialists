ae75c2b388ad57a21b9bd4df1215666a68b18439
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-05-27 00:34:03 -0700

    fixing reviews bug

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -3,11 +3,12 @@ class Api::V1::JobsController < ApplicationController
   rescue_from JSON::ParserError, with: :handle_json_parsing_error
 
   def index
-    # puts "Rendering index action..."
+    puts "Rendering index action..."
     render json: "Northwest Extremity Specialists " * 1000
   end
 
   def pull_google_places_cache
+    puts "Entering pull_google_places_cache method..."
     redis = Redis.new(
       url: ENV['REDIS_URL'],
       ssl: true,
@@ -17,18 +18,18 @@ class Api::V1::JobsController < ApplicationController
   
     cached_reviews = redis.get(cache_key)
     if cached_reviews
-      # puts "Using cached reviews"
+      puts "Using cached reviews"
       reviews = JSON.parse(cached_reviews)
     else
-      # puts "Fetching fresh reviews from Google Places API"
+      puts "Fetching fresh reviews from Google Places API"
       reviews = GooglePlacesCached.fetch_five_star_reviews_for_companies
-      redis.setex(cache_key, 7.days.to_i, reviews.to_json)
+      redis.setex(cache_key, 30.days.to_i, reviews.to_json)
     end
   
     creekside_reviews = reviews["Creekside Physical Therapy"] || []
     northwest_reviews = reviews["Northwest Extremity Specialists"] || []
   
-    # puts "Successfully fetched reviews for Creekside and Northwest"
+    puts "Successfully fetched reviews for Creekside and Northwest"
     csrf_token = form_authenticity_token
     render json: { creekside_reviews: creekside_reviews, northwest_reviews: northwest_reviews, csrf_token: csrf_token }
   end
@@ -37,13 +38,13 @@ class Api::V1::JobsController < ApplicationController
   private
 
   def handle_unexpected_error(error)
-    # puts "Handling unexpected error: #{error.message}"
+    puts "Handling unexpected error: #{error.message}"
     # OfficeMailer.error_email("Unexpected Error", error.message).deliver_later
     render json: { error: "An unexpected error occurred: #{error.message}" }, status: :internal_server_error
   end
 
   def handle_json_parsing_error(error)
-    # puts "Handling JSON parsing error: #{error.message}"
+    puts "Handling JSON parsing error: #{error.message}"
     error_message = "Failed to parse JSON: #{error.message}"
     # OfficeMailer.error_email("JSON Parsing Error", error_message).deliver_later
     render json: { error: error_message }, status: :unprocessable_entity
@@ -58,7 +59,7 @@ class GooglePlacesCached
   require 'openssl'
 
   def self.fetch_five_star_reviews_for_companies
-    # puts "Fetching five-star reviews for companies..."
+    puts "Entering fetch_five_star_reviews_for_companies method..."
     companies = {
       "Creekside Physical Therapy" => ["ChIJT8nUWmzlBIgRnZluSKvaU7o", "ChIJy6GIldiP4okR-sQZEghTDSg", "ChIJt1CU6gxK0IkR4wEmOq3hYr4"],
       "Northwest Extremity Specialists" => ["ChIJf07ARPkJlVQRJCA-9wte444", "ChIJi3RsjPEMlVQRt1cOeU3_g48", "ChIJSRSts-CglVQRfXCyBEPzHNg"]
@@ -72,10 +73,10 @@ class GooglePlacesCached
 
     cached_reviews = redis.get(cache_key)
     if cached_reviews
-      # puts "Using cached reviews"
+      puts "Using cached reviews in fetch_five_star_reviews_for_companies"
       reviews = JSON.parse(cached_reviews)
     else
-      # puts "Fetching fresh reviews from Google Places API"
+      puts "Fetching fresh reviews from Google Places API in fetch_five_star_reviews_for_companies"
       reviews = fetch_reviews_from_google(companies, api_key)
       redis.setex(cache_key, 30.days.to_i, reviews.to_json)
     end
@@ -84,18 +85,19 @@ class GooglePlacesCached
   end
 
   def self.fetch_reviews_from_google(companies, api_key)
+    puts "Entering fetch_reviews_from_google method..."
     reviews = {}
 
     companies.each do |company, place_ids|
-      # puts "Fetching reviews for company: #{company}"
+      puts "Fetching reviews for company: #{company}"
       reviews[company] = place_ids.flat_map do |place_id|
-        # puts "Fetching reviews for place ID: #{place_id}"
+        puts "Fetching reviews for place ID: #{place_id}"
         fetch_five_star_reviews_for_place_id(place_id, api_key)
       end
-      # puts "Successfully fetched reviews for company: #{company}"
+      puts "Successfully fetched reviews for company: #{company}"
     end
 
-    # puts "Finished fetching reviews for all companies"
+    puts "Finished fetching reviews for all companies"
     reviews
   end
 
@@ -111,17 +113,16 @@ class GooglePlacesCached
     data = JSON.parse(response.body)
 
     if data['status'] == 'OK'
-      # puts "Successfully fetched reviews for place ID: #{place_id}"
+      puts "Successfully fetched reviews for place ID: #{place_id}"
       reviews = data['result']['reviews'] || []
       reviews.select { |review| review['rating'] == 5 }.each do |review|
         puts " - #{review['author_name']}: #{review['text']}"
       end
       reviews.select { |review| review['rating'] == 5 }
     else
-      # puts "Error fetching reviews for Place ID #{place_id}: #{data['status']}"
+      puts "Error fetching reviews for Place ID #{place_id}: #{data['status']}"
       Rails.logger.error("Error fetching reviews for Place ID #{place_id}: #{data['status']}")
       []
     end
   end
 end
-
